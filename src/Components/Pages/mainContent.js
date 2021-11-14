ac68ff6776e24dd8940ce0f105aa300d0555520a
cazabe <cazcarlos@hotmail.com>
2021-11-13 23:47:47 -0500

    Refactor & New : Adding new social and skill section, refactorin main content component

--- src/Components/Pages/mainContent.js
+++ src/Components/Pages/mainContent.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Projects from "./projects/index";
-import Knowledge from "./knowledge/index";
+import Skills from "./skills/index";
 import Contact from "./contact/index";
 import programmer from "../../Assets/software_developer.png";
+import youtube from "../../Assets/icons8-youtube-logo.gif";
+import instagram from "../../Assets/icons8-instagram.gif";
+import linkedIn from "../../Assets/icons8-linkedin-circled.gif";
+import github from "../../Assets/icons8-github.gif";
 
 const Content = () => {
   return (
@@ -13,46 +17,46 @@ const Content = () => {
             <strong>Hola, soy Carlos.</strong>
           </h1>
           <p>
-            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
           </p>
           <div className="mb-4 my-button">
             <a href="/images/myw3schoolsimage.jpg" download>
               <strong>Descarga mi cv</strong>
             </a>
           </div>
-          <div className="row text-center">
-            <div className="col-md-1">
-              <a href="https://github.com/cazabe" target="_blank" rel="noreferrer">
-                <span className="fa fa-github"></span>
-              </a>
-            </div>
-            <div className="col-md-1">
-              <a href="https://www.linkedin.com/in/carlos-zavala-benavides" target="_blank" rel="noreferrer">
-                <span className="fa fa-linkedin"></span>
-              </a>
-            </div>
-
-            <div className="col-md-1">
-              <a href="https://www.instagram.com/kamaytech/" target="_blank" rel="noreferrer">
-                <span className="fa fa-youtube"></span>
-              </a>
-            </div>
-
-            <div className="col-md-1">
-              <a href="https://www.instagram.com/kamaytech/" target="_blank" rel="noreferrer">
-                <span className="fa fa-instagram"></span>
-              </a>
-            </div>
-          </div>
         </div>
         <div className="col-md-6 col-xs-12">
-          <img src={programmer} style={{ height: "400px", width: "100%" }} alt="programming" />
+          <img
+            src={programmer}
+            style={{ height: "400px", width: "100%" }}
+            alt="programming"
+          />
         </div>
       </div>
-      <Knowledge />
+      <div class="socials">
+        <a href="#" target="_blank">
+          <img src={youtube} alt="Twitter" loading="lazy" class="socicon" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={instagram} alt="Instagram" loading="lazy" class="socicon" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={linkedIn} alt="Linkedin" loading="lazy" class="socicon" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={github} alt="Github" class="socicon" />
+        </a>
+      </div>
+      <Skills />
       <Projects />
       <Contact />
     </div>
--- src/Components/Pages/projects/index.js
+++ src/Components/Pages/projects/index.js
@@ -3,8 +3,84 @@ import "./projects.css";
 
 const Projects = () => {
   return (
-    <div className="text-center">
-      <h1><strong>Mis proyectos</strong></h1>
+    <div className="row">
+      <section className="projects" id="projects">
+        <h2 className="projects-title">Algunos de mis proyectos</h2>
+        <div className="projects-container">
+          <div className="col-md-4 col-xs-12">
+            <div className="project-container project-card">
+              <img
+                src="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding.jpg"
+                alt="expense-tracker"
+                loading="lazy"
+                className="project-pic"
+              />
+              <h3 className="project-title">Expense Tracker</h3>
+              <p className="project-details">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+                ratione vel inventore labore commodi modi quos culpa aut saepe!
+                Alias!
+              </p>
+              <a
+                href="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding.jpg"
+                target="_blank"
+                className="project-link"
+                rel="noreferrer"
+              >
+                Check it Out
+              </a>
+            </div>
+          </div>
+          <div className="col-md-4 col-xs-12">
+            <div className="project-container project-card">
+              <img
+                src="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding.jpg"
+                alt="netflic-clone"
+                loading="lazy"
+                className="project-pic"
+              />
+              <h3 className="project-title">Netflix Clone</h3>
+              <p className="project-details">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+                ratione vel inventore labore commodi modi quos culpa aut saepe!
+                Alias!
+              </p>
+              <a
+                href="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding.jpg"
+                target="_blank"
+                className="project-link"
+                rel="noreferrer"
+              >
+                Check it Out
+              </a>
+            </div>
+          </div>
+          <div className="col-md-4 col-xs-12">
+            <div className="project-container project-card">
+              <img
+                src="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding.jpg"
+                alt="greeny-earth"
+                loading="lazy"
+                className="project-pic"
+              />
+              <h3 className="project-title">Greeny Earth</h3>
+              <p className="project-details">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+                ratione vel inventore labore commodi modi quos culpa aut saepe!
+                Alias!
+              </p>
+              <a
+                href="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding.jpg"
+                target="_blank"
+                className="project-link"
+                rel="noreferrer"
+              >
+                Check it Out
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
